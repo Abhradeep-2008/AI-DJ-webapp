78128660f2e69486d0897f55fabcb6ce77197ef1
Abhradeep-2008 <74490559+Abhradeep-2008@users.noreply.github.com>
2021-06-09 21:26:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music = "";
 music2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 
 
@@ -15,8 +19,46 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
 
+    posenet = ml5.poseNet(video, ModelLoaded); 
+    posenet.on('pose', gotResults);
 }
 
 function draw(){
     image(video,0,0,500,500)
 }
+
+function ModelLoaded(){
+    console.log("Posenet is Initialized!");
+}
+
+
+function gotResults(results){
+    if(results.length > 0){
+        console.log(results);
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
